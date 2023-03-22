e8df92ebee07aa0a1174eba37018cd3ab908ead8
Sakenuly <sakenuly2016@yandex.ru>
2023-03-22 15:38:13 +0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,13 @@
  import { weatherData } from "./weatherApi.js";
  import { render } from "./render.js";
  import { savedCity } from "./savedCity.js";
- 
+if (JSON.parse(localStorage.getItem(2)) === null){
+    weatherData("Astana")
+    .then(data => {
+        apply(data);
+   })
+}
+if (JSON.parse(localStorage.getItem(2)) !== null) {savedCity()}
 render()
  const submitButton = document.querySelector('.weather__button');
  const favoriteButton = document.querySelector('.bottom-now-content__favorite')
